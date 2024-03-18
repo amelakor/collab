9970f762a7bb0aec18c8c16d249fa7c2950e4e5d
amelakor <amela_korjenic@hotmail.com>
2024-03-18 16:32:32 +0100

    Search

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -6,8 +6,10 @@ import Image from "next/image";
 import { api } from "@/convex/_generated/api";
 import { useOrganization } from "@clerk/nextjs";
 import { useApiMutation } from "@/hooks/use-api-mutation";
+import { useRouter } from "next/navigation";
 
 export const EmptyBoards = () => {
+    const router = useRouter();
     const { organization } = useOrganization();
     const { mutate, pending } = useApiMutation(api.board.create);
 
@@ -19,6 +21,7 @@ export const EmptyBoards = () => {
         })
             .then((id) => {
                 toast.success("Board has been created.");
+                router.push(`/board/${id}`);
             })
             .catch(() => toast.error("Failed to create board"));
     };
