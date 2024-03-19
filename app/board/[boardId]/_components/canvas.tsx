c12c9e7f047638616f4f6a9aff1eaf20bfc416ac
amelakor <amela_korjenic@hotmail.com>
2024-03-19 15:38:33 +0100

    Toolbar options

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -3,19 +3,39 @@
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useSelf } from "@/liveblocks.config";
+import {
+    useCanRedo,
+    useCanUndo,
+    useHistory,
+    useSelf,
+} from "@/liveblocks.config";
+import { useState } from "react";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 
 interface CanvasProps {
     boardId: string;
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
-    const info = useSelf((me) => me.info);
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None,
+    });
+
+    const history = useHistory();
+    const canUndo = useCanUndo();
+    const canRedo = useCanRedo();
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info boardId={boardId} />
             <Participants />
-            <Toolbar />
+            <Toolbar
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                undo={history.undo}
+                redo={history.redo}
+                canRedo={canRedo}
+                canUndo={canUndo}
+            />
         </main>
     );
 };
